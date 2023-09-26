58ef00333edc4f6f3838da43a3a8569cfe36d504
Kali Sina <ali.sina89@yahoo.com>
2023-09-26 14:37:02 +0200

    Initial Commit

---
+++ LidarVideoDepth/LidarVideoDepthApp.swift
@@ -0,0 +1,17 @@
+//
+//  LidarVideoDepthApp.swift
+//  LidarVideoDepth
+//
+//  Created by Triumph on 26/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct LidarVideoDepthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
